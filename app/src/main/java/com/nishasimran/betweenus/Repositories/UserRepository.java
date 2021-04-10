4cbbb9fee67484de4316b9fcae4fe15f657790de
Simran Sharma <simran.s.sharma00@gmail.com>
2021-04-11 05:11:04 +0530

    Added the repository class

---
+++ app/src/main/java/com/nishasimran/betweenus/Repositories/UserRepository.java
@@ -0,0 +1,51 @@
+package com.nishasimran.betweenus.Repositories;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.nishasimran.betweenus.DataClasses.User;
+import com.nishasimran.betweenus.Database.UserDao;
+import com.nishasimran.betweenus.Database.UserRoomDatabase;
+
+import java.util.List;
+
+public class UserRepository {
+    
+    private UserDao userDao;
+    private LiveData<List<User>> allUsers;
+
+    // Note that in order to unit test the UserRepository, you have to remove the Application
+    // dependency. This adds complexity and much more code, and this sample is not about testing.
+    // See the BasicSample in the android-architecture-components repository at
+    // https://github.com/googlesamples
+    UserRepository(Application application) {
+        UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
+        userDao = db.userDao();
+        allUsers = userDao.getAlphabetizedUsers();
+    }
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    LiveData<List<User>> getAllUsers() {
+        return allUsers;
+    }
+
+    // You must call this on a non-UI thread or your app will throw an exception. Room ensures
+    // that you're not doing any long running operations on the main thread, blocking the UI.
+    void insert(User user) {
+        UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.insert(user));
+    }
+
+    void update(User user) {
+        UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.update(user));
+    }
+
+    void delete(User user) {
+        UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.delete(user));
+    }
+
+    void deleteAll() {
+        UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.deleteAll());
+    }
+}
