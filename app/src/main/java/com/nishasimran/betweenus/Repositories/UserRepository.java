35c4be0fb2c0551265c03b7f0ca6d87fca3a596a
Simran Sharma <simran.s.sharma00@gmail.com>
2021-04-11 05:29:09 +0530

    Updates

--- app/src/main/java/com/nishasimran/betweenus/Repositories/UserRepository.java
+++ app/src/main/java/com/nishasimran/betweenus/Repositories/UserRepository.java
@@ -19,7 +19,7 @@ public class UserRepository {
     // dependency. This adds complexity and much more code, and this sample is not about testing.
     // See the BasicSample in the android-architecture-components repository at
     // https://github.com/googlesamples
-    UserRepository(Application application) {
+    public UserRepository(Application application) {
         UserRoomDatabase db = UserRoomDatabase.getDatabase(application);
         userDao = db.userDao();
         allUsers = userDao.getAlphabetizedUsers();
@@ -27,25 +27,25 @@ public class UserRepository {
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
-    LiveData<List<User>> getAllUsers() {
+    public LiveData<List<User>> getAllUsers() {
         return allUsers;
     }
 
     // You must call this on a non-UI thread or your app will throw an exception. Room ensures
     // that you're not doing any long running operations on the main thread, blocking the UI.
-    void insert(User user) {
+    public void insert(User user) {
         UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.insert(user));
     }
 
-    void update(User user) {
+    public void update(User user) {
         UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.update(user));
     }
 
-    void delete(User user) {
+    public void delete(User user) {
         UserRoomDatabase.databaseWriteExecutor.execute(() -> userDao.delete(user));
     }
 
-    void deleteAll() {
+    public void deleteAll() {
         UserRoomDatabase.databaseWriteExecutor.execute(userDao::deleteAll);
     }
 }
