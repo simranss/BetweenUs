8ec3cbb9727a23fcf4b1dffd6ae18a50d6e85caa
Simran Sharma <simran.s.sharma00@gmail.com>
2021-04-17 12:22:21 +0530

    Added new strings related to shared preferences

--- app/src/main/java/com/nishasimran/betweenus/Strings/CommonStrings.java
+++ app/src/main/java/com/nishasimran/betweenus/Strings/CommonStrings.java
@@ -2,6 +2,8 @@ package com.nishasimran.betweenus.Strings;
 
 public class CommonStrings {
 
+    public final static String NULL = "null";
+
     public final static String MY_PRIVATE_KEY = "my_private_key";
     public final static String MY_PUBLIC_KEY = "my_public_key";
     public final static String SERVER_KEY = "server_key";
@@ -13,5 +15,11 @@ public class CommonStrings {
     public final static String STATUS_SENT = "status_1";
     public final static String STATUS_DELIVERED = "status_2";
     public final static String STATUS_SEEN = "status_3";
+
+    public final static String SHARED_PREFERENCE = "MyPreferences";
+    public final static String SHARED_PREFERENCE_UID = "uid";
+    public final static String SHARED_PREFERENCE_SERVER_UID = "server_uid";
+    public final static String SHARED_PREFERENCE_STATE = "state";
+    public final static String SHARED_PREFERENCE_SERVER_LAST_SEEN = "server_last_seen";
 
 }
