3a1095396a1e532489e5d6289dc08c631085c95a
Simran Sharma <simran.s.sharma00@gmail.com>
2021-04-17 17:46:23 +0530

    Added new file for strings related to firebase

---
+++ app/src/main/java/com/nishasimran/betweenus/Strings/FirebaseStrings.java
@@ -0,0 +1,9 @@
+package com.nishasimran.betweenus.Strings;
+
+public class FirebaseStrings {
+
+    public final static String USERS = "users";
+    public final static String LAST_SEEN = "last_seen";
+    public final static String PUBLIC_KEY = "P";
+    public final static String NETWORK_STATE_PATH = ".info/connected";
+}
