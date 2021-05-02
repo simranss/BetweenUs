10820e077804755e74a5b08858b7ea38449904e9
Simran Sharma <simran.s.sharma00@gmail.com>
2021-05-02 12:37:39 +0530

    Added required basic methods

--- app/src/main/java/com/nishasimran/betweenus/Fragments/MainFragment.java
+++ app/src/main/java/com/nishasimran/betweenus/Fragments/MainFragment.java
@@ -1,5 +1,6 @@
 package com.nishasimran.betweenus.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nishasimran.betweenus.Activities.MainActivity;
+import com.nishasimran.betweenus.DataClasses.Key;
+import com.nishasimran.betweenus.DataClasses.User;
+import com.nishasimran.betweenus.Firebase.FirebaseDb;
 import com.nishasimran.betweenus.R;
 
 public class MainFragment extends Fragment {
 
-    public MainFragment() {
-        // Required empty public constructor
+    private final String TAG = "MainFrag";
+
+    private final Activity activity;
+
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    public MainFragment(Activity activity) {
+        this.activity = activity;
     }
 
     @Override
@@ -25,6 +42,44 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        FirebaseDb.getInstance().goOnline();
+        if (isInternetAvailable()) {
+            restartConnectionChangeListener();
+        }
+
+        return view;
+    }
+
+    private boolean isInternetAvailable() {
+        if (activity instanceof MainActivity) {
+            return ((MainActivity) activity).isInternetAvail();
+        }
+        return false;
+    }
+
+    private void restartConnectionChangeListener() {
+        if (activity instanceof MainActivity) {
+            ((MainActivity) activity).restartListenerForConnectionChange();
+        }
+    }
+
+    private void updateState(String state) {
+        if (activity instanceof MainActivity) {
+            ((MainActivity) activity).updateState(state);
+        }
+    }
+
+    private void insertUser(User user) {
+        if (activity instanceof MainActivity) {
+            ((MainActivity) activity).insertUser(user);
+        }
+    }
+
+    private void insertKey(Key key) {
+        if (activity instanceof MainActivity) {
+            ((MainActivity) activity).insertKey(key);
+        }
     }
 }
