7383db73a53f23627f8c918b92f9250d760f2629
Simran Sharma <simran.s.sharma00@gmail.com>
2021-06-03 06:08:48 +0530

    Added the three fragments for navigation drawer

---
+++ app/src/main/java/com/nishasimran/betweenus/Fragments/ChatFragment.java
@@ -0,0 +1,33 @@
+package com.nishasimran.betweenus.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nishasimran.betweenus.R;
+
+public class ChatFragment extends Fragment {
+
+    private final MainFragment mainFragment;
+
+    public ChatFragment(MainFragment fragment) {
+        this.mainFragment = fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View parent = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        return parent;
+    }
+}
---
+++ app/src/main/java/com/nishasimran/betweenus/Fragments/SettingsFragment.java
@@ -0,0 +1,33 @@
+package com.nishasimran.betweenus.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nishasimran.betweenus.R;
+
+public class SettingsFragment extends Fragment {
+
+    private final MainFragment mainFragment;
+
+    public SettingsFragment(MainFragment fragment) {
+        mainFragment = fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View parent = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return parent;
+    }
+}
