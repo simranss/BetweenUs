225e054b27a2c4f8d9466a446295f0d423b8edd9
Simran Sharma <simran.s.sharma00@gmail.com>
2021-06-04 14:46:59 +0530

    Chat fragment updated basic layout

--- app/src/main/java/com/nishasimran/betweenus/Fragments/ChatFragment.java
+++ app/src/main/java/com/nishasimran/betweenus/Fragments/ChatFragment.java
@@ -1,18 +1,31 @@
 package com.nishasimran.betweenus.Fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 
 import com.nishasimran.betweenus.R;
+import com.nishasimran.betweenus.Utils.BlurBuilder;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ChatFragment extends Fragment {
 
     private final MainFragment mainFragment;
+
+    private ConstraintLayout root;
+    private ImageView navOpen;
+
+    private ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener;
 
     public ChatFragment(MainFragment fragment) {
         this.mainFragment = fragment;
@@ -24,7 +37,7 @@ public class ChatFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (mainFragment.isDocsExpanded()) {
             mainFragment.setDocsExpanded(false);
         }
@@ -32,6 +45,51 @@ public class ChatFragment extends Fragment {
         // Inflate the layout for this fragment
         View parent = inflater.inflate(R.layout.fragment_chat, container, false);
 
+        initViews(parent);
+
+        listenersForViews();
+
+        initOnGlobalLayoutListener();
+
+        blurBackground();
+
+
+
         return parent;
+    }
+
+    private void listenersForViews() {
+        navOpen.setOnClickListener(v -> mainFragment.openDrawer());
+    }
+
+    private void initViews(View parent) {
+        root = parent.findViewById(R.id.chat_root);
+        navOpen = parent.findViewById(R.id.chat_nav_open);
+
+    }
+
+    private void blurBackground() {
+        if (getContext() != null) {
+            if (root.getWidth() > 0) {
+                Bitmap blurredBitmap = BlurBuilder.blur(getContext(), R.drawable.background_01_img, 0.9f, 3f);
+                root.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+            } else {
+                root.getViewTreeObserver().addOnGlobalLayoutListener(onGlobalLayoutListener);
+            }
+        }
+    }
+
+    private void initOnGlobalLayoutListener() {
+        if (getContext() != null)
+            onGlobalLayoutListener = () -> {
+                Bitmap blurredBitmap = BlurBuilder.blur(getContext(), R.drawable.background_01_img, 0.9f, 3f);
+                root.setBackground(new BitmapDrawable(getResources(), blurredBitmap));
+            };
+    }
+
+    @Override
+    public void onDestroyView() {
+        root.getViewTreeObserver().removeOnGlobalLayoutListener(onGlobalLayoutListener);
+        super.onDestroyView();
     }
 }
