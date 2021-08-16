bacf2fd595330ec611eb191b19e9448914a0287c
Simran Sharma <simran.s.sharma00@gmail.com>
2021-08-17 03:50:50 +0530

    Added code for encrypting and decrypting image

--- app/src/main/java/com/nishasimran/betweenus/Values/EncryptionString.java
+++ app/src/main/java/com/nishasimran/betweenus/Values/EncryptionString.java
@@ -4,4 +4,6 @@ public class EncryptionString {
 
     public final static String TRANSFORMATION_AES = "AES/CBC/PKCS5PADDING";
     public final static String ALGORITHM_AES = "AES";
+    public final static String TRANSFORMATION_ARC4 = "ARC4/NONE/NoPadding";
+    public final static String ALGORITHM_ARC4 = "ARC4";
 }
