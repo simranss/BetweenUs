f956058ada8766c09f36ddbef9b92cb80c3bfa1f
simran <simran.s.sharma00@gmail.com>
2021-09-10 18:48:59 +0530

    Added a utility for images

---
+++ app/src/main/java/com/nishasimran/betweenus/Utils/ImageUtil.java
@@ -0,0 +1,45 @@
+package com.nishasimran.betweenus.Utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtil {
+
+    public static Bitmap convert(byte[] decodedBytes) throws IllegalArgumentException
+    {
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public static byte[] convert(Bitmap bitmap)
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        return outputStream.toByteArray();
+    }
+
+    public static Bitmap convert(String base64Str) throws IllegalArgumentException
+    {
+        Log.d("TAG", "convert: " + base64Str);
+        byte[] decodedBytes = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",")  + 1),
+                Base64.DEFAULT
+        );
+
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public static String convertToStr(Bitmap bitmap)
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        String string = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+        Log.d("TAG", "convertToStr: " + string);
+        return string;
+    }
+}
